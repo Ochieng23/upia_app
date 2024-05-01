5c8c56f61c7e6a72410817d40bafe80ebf9d3c1a
paul <ochiengpaul001@gmail.com>
2024-05-01 19:15:31 +0300

    deploy version 1

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,6 +3,6 @@ import logo from "../images/images/UPIALogo-01.svg"
 
 export function Logo(props) {
   return (
-    <Image src={logo} alt="Logo" width={130} height={130} />
+    <Image src={logo} alt="Logo" width={150} height={150} />
   )
 }
